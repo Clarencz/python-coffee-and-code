58e162fbe0d35e2d900a010681b34be954bd8339
clarence <cmabeya3@gmail.com>
2025-09-13 10:34:03 +0300

    store data using serializer

--- python_django/api/views.py
+++ python_django/api/views.py
@@ -8,7 +8,7 @@ from rest_framework.decorators import api_view
 
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def studentView(request):
     # """manual serialization"""
     # students = Student.objects.all()
@@ -20,3 +20,9 @@ def studentView(request):
         students =Student.objects.all()
         serializer = StudentSerializer(students,many = True)
         return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
