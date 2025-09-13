f8d0de4fe090b984c7cac2cc4de1f4099fd53ec3
clarence <cmabeya3@gmail.com>
2025-09-13 11:42:18 +0300

    update,delete employee-class based views

--- python_django/api/views.py
+++ python_django/api/views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from employees.models import Employee
+from django.http import Http404
 
 
 # Create your views here.
@@ -65,3 +66,24 @@ class Employees(APIView):
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
+class EmployeeDetail(APIView):
+    def get_object(self,pk):
+        try:
+            employee = Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            raise Http404
+    def get(self,request,pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data,status = status.HTTP_200_OK)
+    def put(self,request,pk):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee,data= request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status = status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def delete(self,request,pk):
+        employee = self.get_object(pk)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
